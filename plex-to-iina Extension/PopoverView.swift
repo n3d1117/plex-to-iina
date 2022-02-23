86d6ea04c49d7dc026899f287379789500b7b338
StevenRCE0 <40051361+StevenRCE0@users.noreply.github.com>
2022-02-23 13:59:01 +0800

    Fallback support for macOS Catalina

--- plex-to-iina Extension/PopoverView.swift
+++ plex-to-iina Extension/PopoverView.swift
@@ -21,16 +21,33 @@ class PopoverViewWrapper: SFSafariExtensionViewController {
 
 }
 
+private let ProjectURL = "https://github.com/n3d1117/plex-to-iina"
+
 struct PopoverView: View {
     var body: some View {
         VStack(spacing: 5) {
-            Link("plex-to-iina", destination: URL(string: "https://github.com/n3d1117/plex-to-iina")!)
-                .font(.title)
+            if #available(OSXApplicationExtension 11.0, *) {
+                Link("plex-to-iina", destination: URL(string: ProjectURL)!)
+                    .font(.title)
+            } else {
+                // Fallback on earlier versions
+                Button("plex-to-iina", action: {
+                    () -> Void
+                    in
+                    NSWorkspace.shared.open(URL(string: ProjectURL)!)
+                })
+                    .buttonStyle(PlainButtonStyle())
+                    .font(.title)
+                    .foregroundColor(.blue)
+            }
             HStack(spacing: 5) {
                 Text("made with")
-                Image(systemName: "heart.fill")
-                    .imageScale(.small)
-                    .foregroundColor(.red)
+                if #available(OSXApplicationExtension 11.0, *) {
+                    Image(systemName: "heart.fill")
+                } else {
+                    Text("􀊵")
+                        .foregroundColor(.red)
+                }
                 Text("by ned")
             }
         }.padding(20)
